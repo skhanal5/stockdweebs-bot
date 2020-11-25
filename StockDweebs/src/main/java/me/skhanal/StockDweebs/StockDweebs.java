a4639cc3926bcae79fc98528445cc44e0ea1bd5f
Subodh Khanal <74752121+skhanal5@users.noreply.github.com>
2020-11-24 19:45:16 -0500

    Project initialization
    Created project and encapsulated oath token

--- StockDweebs/src/main/java/me/skhanal/StockDweebs/StockDweebs.java
+++ StockDweebs/src/main/java/me/skhanal/StockDweebs/StockDweebs.java
@@ -1,19 +1,43 @@
 package me.skhanal.StockDweebs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
 import javax.security.auth.login.LoginException;
-
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class StockDweebs {
-	
 	public static void main(String[]args) {
-		JDABuilder jdabuilder = JDABuilder.createDefault("NzgwNjkxMTAwOTY0MzU2MTQ2.X7yxPw.9VvTAS43w6hQkvM8ZLmngDcHn4U");
+	
+		JSONParser parser = new JSONParser();
+		String token = "";
+		JSONObject jsonObject = null;
 		
 		try {
-			jdabuilder.build();
-		} catch (LoginException e){
+			jsonObject = (JSONObject) parser.parse(new FileReader("C:\\Users\\subod\\Documents\\GitHub\\stockdweebs-bot\\StockDweebs\\src\\main\\java\\me\\skhanal\\StockDweebs\\token.json"));
+			token = (String) jsonObject.get("token");
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(ParseException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
+		
+		JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+		JDA jda = null;
+		
+		try {
+			jda = jdaBuilder.build();
+		} catch (LoginException e){
+			e.printStackTrace();
+		} 
+	} 
 	
 }
